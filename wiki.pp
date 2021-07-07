5fd2e2de30afa6b16de743e5c298dec0400bf434
andri37 <rabarisoa@et.esiea.fr>
2021-07-07 13:57:13 +0000

    wiki3

--- wiki.pp
+++ wiki.pp
@@ -5,11 +5,18 @@ package {
     ensure => present;
 }
 
-exec {'Download doc wiki':
-  command  => 'wget -O /usr/src/dokuwiki.tgz \
-  https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
-  path     => ['/usr/bin'],
+file {'get dokuwiki':
+  ensure   => 'present',
+  source   => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+  path     => '/usr/src/dokuwiki.tgz',
 }
+
+#exec {'Download doc wiki':
+#  command  => 'wget -O /usr/src/dokuwiki.tgz \
+#  https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+#  path     => ['/usr/bin'],
+#}
+
 exec {'decompress file':
   cwd      => '/usr/src',
   command  => 'tar xavf dokuwiki.tgz && mv dokuwiki-2020-07-29 dokuwiki',

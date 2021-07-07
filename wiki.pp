8cc52a264f48005ccdcddef6b998082ff09b0131
andri37 <rabarisoa@et.esiea.fr>
2021-07-07 14:21:09 +0000

    wiki_remove

--- wiki.pp
+++ wiki.pp
@@ -17,8 +17,14 @@ file {'get dokuwiki':
 #  path     => ['/usr/bin'],
 #}
 
-exec {'decompress file':
-  cwd      => '/usr/src',
-  command  => 'tar xavf dokuwiki.tgz && mv dokuwiki-2020-07-29 dokuwiki',
-  path     => ['/usr/bin'],
+exec {
+  'decompress file':
+    cwd     => '/usr/src',
+    command => 'tar xavf dokuwiki.tgz && mv dokuwiki-2020-07-29 dokuwiki',
+    path    => '/usr/bin';
+ 
+  'remove tgz':
+    cwd     => '/usr/src',
+    path    => '/usr/bin',
+    command => 'sudo rm -r dokuwiki.tgz';
 }

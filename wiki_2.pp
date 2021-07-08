033979d3087bdc19eb4584c32179c6766cd5802e
andri37 <rabarisoa@et.esiea.fr>
2021-07-08 10:19:48 +0000

    wiki2.1

--- wiki_2.pp
+++ wiki_2.pp
@@ -1,6 +1,6 @@
 package {
   'apache2':
-    ensure => present;
+    ensure => present; 
   'php7.3':
     ensure => present;
 }
@@ -39,4 +39,62 @@ file {
     recurse => true,
     force   => true,
     require => File['rename-dokuwiki'],
+}
+
+file { 
+  '/var/www/recettes':
+    ensure  => 'directory',
+    owner   => 'www-data',
+    mode    => '0755',
+    source  => '/usr/src/dokuwiki/',
+    recurse => true,    
+}
+
+file {
+  '/var/www/politique':
+    ensure  => 'directory',
+    owner   => 'www-data',
+    mode    => '0755',
+    source  => '/usr/src/dokuwiki/',
+    recurse => true,
+}
+
+file {
+  '/etc/apache2/sites-available/politique-wiki.conf':
+    source  => '/etc/apache2/sites-available/000-default.conf',
+}
+
+file {
+  '/etc/apache2/sites-available/recettes-wiki.conf':
+    source  => '/etc/apache2/sites-available/000-default.conf',
+}
+
+exec {
+  'conf-change-1':
+    command => 'sed -i \'s/html/politique-wiki/g\' /etc/apache2/sites-available/politique-wiki.conf && sed -i \'s/html/recettes-wiki/g\' /etc/apache2/sites-available/recettes-wiki.conf',
+    path    => ['/usr/bin','/usr/sbin'],
+}
+
+exec {
+  'conf-change-2':
+    command => 'sed -i \'s/*:80/*:1080/g\' /etc/apache2/sites-available/politique-wiki.conf && sed -i \'s/*:80/*:1080/g\' /etc/apache2/sites-available/recettes-wiki.conf',
+    path    => ['/usr/bin','/usr/sbin'],
+}
+
+exec { 
+  'enable-vhost-1':
+    command => 'a2ensite politique-wiki',
+    path    => '/usr/bin',
+}
+
+exec {
+  'enable-vhost-2':
+    command => 'a2ensite recettes-wiki',
+    path    => ['/usr/bin', '/usr/sbin'],
+}
+
+service { 
+  'apache2':
+    ensure    => running,
+    subscribe => [Exec['enable-vhost-1'],Exec['enable-vhost-2']], 
 }

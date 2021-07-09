792abb1bd39bcd6abf20b69f308654f32fcac3c9
andri37 <rabarisoa@et.esiea.fr>
2021-07-09 08:15:44 +0000

    0907changes

--- demo_classes.pp
+++ demo_classes.pp
@@ -1,3 +1,7 @@
+$src_dir = '/usr/src'
+$dokuwiki_archive = "${src_dir}/dokuwiki.tgz"
+$dokuwiki_dir = "${src_dir}/dokuwiki-2020-07-29"
+
 class development {
   package { 
     'vim':
@@ -18,15 +22,72 @@ class hosting {
   }
 }
 
+class dokuwiki::download {
+  file {
+    '/usr/src/dokuwiki.tgz':
+      ensure => 'present',
+      source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+  }
+
+  exec {
+    'dokuwiki::unarchive':
+      cwd     => "${src_dir}",
+      command => "tar xavf ${dokuwiki_archive}",
+      creates => "${dokuwiki_dir}",
+      path    => ['/bin'],
+      require => File["${dokuwiki_archive}"],
+  }
+
+  file {
+    '/var/www/${site_dir}':
+      ensure  => 'directory',
+      owner   => 'www-data',
+      group   => 'www-data',
+      # mode    => '0755',
+      source  => "${dokuwiki_dir}",
+      recurse => true;
+
+    '/etc/apache2/sites-available/${site_dir}.conf':
+      ensure  => present,
+      content => template('/home/vagrant/puppet/template/site.conf.erb'),
+      require => [Package['apache2'],
+                File['/var/www/${site_dir}']];
+  }
+
+  exec {
+    'enable-vhost':
+      command => 'a2ensite ${site_dir}',
+      path    => ['/usr/bin', '/usr/sbin'],
+      require => [File['/etc/apache2/sites-available/${site_dir}.conf'],
+                  Package['apache2']];
+  }
+
+  service {
+    'apache2':
+      ensure    => running,
+      subscribe => Exec['enable-vhost'],
+  }
+
+  host {
+    '${site_hostname}':
+      ip => '127.0.0.1';
+  }
+}
 
 node 'control' { 
-  include development
+  #include development
 }
 
 node 'server0' {
+  $site_hostname = 'politique.wiki'
+  $site_dir = 'politique-wiki'
   include hosting
+  include dokuwiki::download
 }
 
 node 'server1' {
+  $site_hostname = 'recettes.wiki'
+  $site_dir = 'recettes-wiki'
   include hosting
+  include dokuwiki::download
 }
